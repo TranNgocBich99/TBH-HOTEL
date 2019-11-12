a942b34d75be731e9d970eaa8992464dd154d8bb
JreamOQ <huanmzvina@gmail.com>
2019-11-12 22:57:14 +0700

    Revert "Cấu trúc Backend"
    This reverts commit 14ff7b1233c33e5b5340c88a6ee3d1ad5d9538f7.

--- cnpm/controller/room.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Room extends Base_controller{
-	public function __construct(){
-		parent:: __construct();
-	}
-
-}
-?>
--- cnpm/model/room.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Room_Model extends Base_Model{
-	public function __construct(){
-		parent:: __construct();
-	}
-
-}
-?>
