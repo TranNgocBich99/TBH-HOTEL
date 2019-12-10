c865517c19d371a4cb65d9d982b67438c6b3e029
Nguyen Thi Hue <nguyenthihue.jvb@gmail.com>
2019-12-10 13:07:59 +0700

    Upload file ảnh

--- hotel/dashboard/controller/category.php
+++ hotel/dashboard/controller/category.php
@@ -18,4 +18,29 @@ class Category extends Base_controller{
 		}
 		header('location: ' . ue_get_admin_link('category', 'listCategory'));
 	}
+	public function uploadFile(){
+		if(!empty($_FILES) && isset($_FILES['thumb'])){
+			$uploadDir = SITEPATH . 'assets/uploads/';
+			$file_name = $_FILES['thumb']['name'];
+			$file_type = $_FILES['thumb']['type'];
+			
+			$file_size = $_FILES['thumb']['size'];
+			$file_temp = $_FILES['thumb']['tmp_name'];
+
+			$ext = pathinfo($file_name, PATHINFO_EXTENSION);
+
+			$file_name = str_replace('.' . $ext, '', $file_name);
+
+			$file_name = $file_name  . '_' . time() . '.' . $ext;
+
+
+			$target_file = $uploadDir . basename($file_name);
+
+			if (move_uploaded_file($file_temp, $target_file)) {
+		    	return $file_name;
+		    }else{
+		    	return '';
+		    }
+		}
+	}
 }
