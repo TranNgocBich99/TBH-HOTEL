999fec615d2eeae8ab0b87c391dba91f80ba824d
Nguyen Thi Hue <nguyenthihue.jvb@gmail.com>
2019-12-11 16:17:03 +0700

    Xem chi tiết phòng & fix css

--- hotel/view/room/detail.php
+++ hotel/view/room/detail.php
@@ -4,29 +4,26 @@ if(!empty($res)){
 	<p class="" style="color: #bf0000; font-size: 22px; font-weight: 600; text-align: center;margin-top: 40px;">Chi tiết phòng</p>
 
 <div class="container">
-<div class="row mt-5 info">
-	<div class="col-sm-3">
-		<img src="<?php echo SITEURL . '/assets/' . $res['thumb']; ?>" width="200px" height="150xp;">
+<div class="row mt-5 info-detail info">
+	<div class="col-sm-4">
+		<img src="<?php echo SITEURL . '/assets/' . $res['thumb']; ?>" height="238">
 	</div>
-	<div class="col-sm-5">
+	<div class="col-sm-8">
 		<p class="room"><?php echo $res['room_name']; ?></p>
 		<div class="info-room mb-2">
-			<ul>
+			<ul class="people">
 				<li><i class="fas fa-female"></i> <?php echo $res['adults']; ?> người lớn</li>
 				<li><i class="fas fa-child"></i> <?php echo $res['children']; ?> trẻ em</li>
 			</ul>
 		</div>
-		<p class="star"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></p>
+		<p class="conf"><?php echo $res['confirmation']; ?></p>
 		<div class="des">
 			<ul>
-				<li><p><a href="">Chi tiết <i class="fas fa-caret-right"></i></a></p></li>
-				<li><p class="price">Giá: <?php echo ue_format_price($res['price']); ?>đ</p></li>
+				<li><p class="price1">Giá: <?php echo ue_format_price($res['price']); ?></p></li>
+				<li><p><button type="button" class="btn btn-dark book-room detail-book">Đặt phòng</button></p></li>
 			</ul>
 		</div>
 		
-	</div>
-	<div class="col-sm-4">
-		<button type="button" class="btn btn-dark book-room">Đặt phòng</button>
 	</div>
 </div>
  </div>
