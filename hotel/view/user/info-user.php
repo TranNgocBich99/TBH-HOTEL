7c6c8ea06258b5bb0a9e110f957829e204d946a6
Nguyen Thi Hue <nguyenthihue.jvb@gmail.com>
2020-04-28 10:52:21 +0700

    Đặt phòng khi chưa login

---
+++ hotel/view/user/info-user.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="<?php ue_assets('css/login.css') ?>">
+<div class="container dkya">
+	<form method="post" action="">
+		<div class=" text-center dk"><b> ĐĂNG NHẬP </b></div>
+
+		<div class=" email"><b>Tài khoản</b></div>
+
+
+		<div class=" text-center place"><input type="name" name="username" placeholder="Nhập tên tài khoản"> </div>
+		<div class="email"><input type="text" name="mail" placeholder="Email"></div>
+		<div class="country"><input type="text" name="" placeholder="Quốc gia"></div>
+		<div class="numberphone"><input type="text" name="" placeholder="Số điện thoại"></div>
+		<div class=" text-center">
+			<button class="btn btn-primary dky" name="login_action"> <b>ĐĂNG NHẬP</b> </button>
+		</div>
+	</form>
+</div>
